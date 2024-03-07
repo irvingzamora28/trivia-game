385abad73874603745dcd7d34a8eb8fbca00bdbd
Irving Zamora <irvingzamora28@gmail.com>
2024-03-07 10:52:06 -0600

    feat: Add trivia sound effect and play it when quiz starts

--- src/components/QuizContainer.tsx
+++ src/components/QuizContainer.tsx
@@ -1,11 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import Quiz from './Quiz';
 import StartButton from '../components/ButtonStartQuiz';
-import triviaQuestions from "../data/trivia.json"; // Import trivia questions
+import triviaSound from "../assets/audio/trivia-sound.mp3";
+import triviaQuestions from "../data/trivia.json";
 
 const QuizContainer: React.FC = () => {
   const [quizStarted, setQuizStarted] = useState<boolean>(false);
+
+  // Play audio when the trivia starts
+  useEffect(() => {
+    if (quizStarted) {
+      const audio = new Audio(triviaSound);
+      audio.volume = 0.1;
+      audio.play().catch(error => console.error("Error playing the audio", error));
+    }
+  }, [quizStarted]); // This effect depends on quizStarted
 
   return (
     <motion.div
@@ -17,7 +27,7 @@ const QuizContainer: React.FC = () => {
       {!quizStarted ? (
         <StartButton onStart={() => setQuizStarted(true)} />
       ) : (
-        <Quiz triviaQuestions={triviaQuestions} /> // Pass trivia questions as a prop
+        <Quiz triviaQuestions={triviaQuestions} />
       )}
     </motion.div>
   );
