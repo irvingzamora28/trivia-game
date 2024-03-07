39dc0a9a1ca3f77d4ce77cab804cbc9262779dd5
Irving Zamora <irvingzamora28@gmail.com>
2024-03-07 14:58:11 -0600

    feat: Improve progress bar visibility and animation control. Add logic to manage progress bar animation timer based on user input and visibility state to enhance user experience.

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,5 +1,4 @@
-// ProgressBar.tsx
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { motion, useAnimation } from "framer-motion";
 
 interface ProgressBarProps {
@@ -14,6 +13,7 @@ const ProgressBar: React.FC<ProgressBarProps> = ({
   timeLimit,
 }) => {
   const controls = useAnimation();
+  const [isVisible, setIsVisible] = useState(true);
 
   const progressBarVariants = {
     initial: { width: "100%" },
@@ -22,18 +22,40 @@ const ProgressBar: React.FC<ProgressBarProps> = ({
       transition: { duration: timeLimit, ease: "linear" },
     },
   };
-
   useEffect(() => {
+    let timerId: any;
+
     if (isProgressBarAnimating) {
       controls.start("animate");
+      setIsVisible(true);
+      // Clear existing timer if there is one
+      if (timerId) {
+        clearTimeout(timerId);
+      }
+      // Set a timer to match the duration of the progress bar animation
+      timerId = setTimeout(() => {
+        setIsVisible(false);
+      }, timeLimit * 1000); // Convert timeLimit to milliseconds
     } else {
-      controls.set("initial");
+      // Immediately make the progress bar visible if not animating
+      setIsVisible(true);
     }
-  }, [isProgressBarAnimating, controls]);
+
+    return () => {
+      if (timerId) {
+        clearTimeout(timerId);
+      }
+    };
+  }, [isProgressBarAnimating, controls, timeLimit]);
 
   return (
     <div className="flex flex-row justify-between w-full m-8 px-8">
-      <div className="relative w-full h-8 bg-slate-100 border-slate-200 border-2 rounded-full overflow-hidden round-shadow">
+      <div
+        className={`relative w-full h-8 bg-slate-100 border-slate-200 border-2 rounded-full overflow-hidden shadow ${
+          isVisible ? "opacity-100" : "opacity-0"
+        }`}
+        style={{ transition: "opacity 0.5s ease-out" }}
+      >
         <motion.div
           key={index}
           className="absolute top-0 left-0 h-full bg-red-500 rounded-full shadow-md"
@@ -41,7 +63,7 @@ const ProgressBar: React.FC<ProgressBarProps> = ({
           initial="initial"
           animate={isProgressBarAnimating ? "animate" : "initial"}
           style={{
-            backgroundSize: '200% 100%',
+            backgroundSize: "200% 100%",
             backgroundImage: `
               repeating-linear-gradient(
                 45deg,
@@ -51,9 +73,10 @@ const ProgressBar: React.FC<ProgressBarProps> = ({
                 rgba(0, 220, 0, 0.7) 20px
               )
             `,
-            animation: isProgressBarAnimating ? 'moveStripes 50s linear infinite' : 'none', // sync with the bar depletion
+            animation: isProgressBarAnimating
+              ? "moveStripes 50s linear infinite"
+              : "none",
           }}
-          
         />
       </div>
     </div>
