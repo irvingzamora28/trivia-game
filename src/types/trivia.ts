810d3656cde037953f755c81237542f6fcf760ca
Irving Zamora <irvingzamora28@gmail.com>
2024-03-09 00:53:27 -0600

    feat: Enhance trivia questions format with option keys and text, adding as optional image, audio and video

--- src/types/trivia.ts
+++ src/types/trivia.ts
@@ -1,9 +1,19 @@
+type OptionKey = "A" | "B" | "C" | "D";
+
+export interface Option {
+  key: OptionKey;
+  text: string;
+  image?: string;
+  audio?: string;
+  video?: string;
+}
+
 export interface TriviaQuestion {
-	id: number;
-	question: string;
-	audio_question: string;
-	image: string;
-	options: string[];
-	answer: string;
-	audio_answer?: string;
+  id: number;
+  question: string;
+  audio_question: string;
+  image: string;
+  options: Option[];
+  answer: OptionKey;
+  audio_answer?: string;
 }
