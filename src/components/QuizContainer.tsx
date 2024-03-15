f153116302bfac0fae94eb0fd5ea3d1bcd61a004
Irving Zamora <irvingzamora28@gmail.com>
2024-03-14 20:21:37 -0600

    feat: Update quiz start logic and button visibility in QuizContainer

--- src/components/QuizContainer.tsx
+++ src/components/QuizContainer.tsx
@@ -11,24 +11,22 @@ const QuizContainer: React.FC = () => {
   const [quizStarted, setQuizStarted] = useState<boolean>(false);
   const [wyrStarted, setWyrStarted] = useState<boolean>(false);
 
-  // Play audio when the trivia starts
   useEffect(() => {
-    if (quizStarted) {
+    if (quizStarted || wyrStarted) {
       const audio = new Audio(triviaSound);
       audio.volume = 0.1;
-      audio
-        .play()
-        .catch((error) => console.error("Error playing the audio", error));
+      audio.play().catch((error) => console.error("Error playing the audio", error));
     }
-  }, [quizStarted]); // This effect depends on quizStarted
+  }, [quizStarted, wyrStarted]); // This effect now depends on both quizStarted and wyrStarted
 
   const handleStart = (type: string) => {
-    // 2-second delay before starting the quiz
     setTimeout(() => {
       if (type === "quiz") {
         setQuizStarted(true);
+        setWyrStarted(false); // Ensure WYR quiz is not started
       } else if (type === "wyr") {
         setWyrStarted(true);
+        setQuizStarted(false); // Ensure Trivia quiz is not started
       }
     }, 2000);
   };
@@ -41,26 +39,20 @@ const QuizContainer: React.FC = () => {
       className="min-h-screen w-screen flex flex-col justify-center items-center gap-4"
     >
       <div className="flex flex-col items-center space-y-4">
-        {" "}
-        {/* Container with spacing */}
-        {!quizStarted ? (
-          <StartButton
-            onStart={() => handleStart("quiz")}
-            text="Start Trivia"
-          />
-        ) : (
+        {!quizStarted && !wyrStarted && ( // Only show buttons if neither quiz has started
+          <>
+            <StartButton onStart={() => handleStart("quiz")} text="Start Trivia" />
+            <StartButton onStart={() => handleStart("wyr")} text="Start Would You Rather" color="lime" />
+          </>
+        )}
+
+        {quizStarted && ( // Show Quiz if it's started
           <Quiz
             triviaPath={trivia.data.file_path}
             triviaQuestions={trivia.questions as TriviaQuestion[]}
           />
         )}
-        {!wyrStarted ? (
-          <StartButton
-            onStart={() => handleStart("wyr")}
-            text="Start Would You Rather"
-            color="lime"
-          />
-        ) : (
+        {wyrStarted && ( // Show WouldYouRatherQuiz if it's started
           <WouldYouRatherQuiz
             triviaPath={trivia.data.file_path}
             triviaQuestions={trivia.questions as TriviaQuestion[]}
