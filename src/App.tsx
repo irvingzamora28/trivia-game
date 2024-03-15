dbaf162a063d0a36b183885d9ec5bc3661fdd166
Irving Zamora <irvingzamora28@gmail.com>
2024-03-15 00:26:41 -0600

    feat: Add background video to App component and update styles for better style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import QuizContainer from "./components/QuizContainer";
+import background from "../src/assets/video/blue-background-spiral.mp4"
 
 function App() {
   return (
-    <div className="App h-screen bg-gradient-to-r from-violet-900 to-blue-600">
-      <QuizContainer />
+    <div className="App h-screen w-screen overflow-hidden relative">
+      <video autoPlay loop muted playsInline className="absolute w-full h-full object-cover">
+        <source src={background} type="video/mp4" />
+        Tu navegador no soporta videos HTML5.
+      </video>
+      <div className="w-full h-full absolute flex justify-center items-center">
+        <QuizContainer />
+      </div>
     </div>
   );
 }
