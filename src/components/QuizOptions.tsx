102bbbf16659e63818d133cbff8d546b05c85850
Irving Zamora <irvingzamora28@gmail.com>
2024-03-19 01:30:33 -0600

    feat: Add animated card flipping and floating effect in QuizOptions component for ChooseAnOptionQuiz

--- src/components/QuizOptions.tsx
+++ src/components/QuizOptions.tsx
@@ -19,7 +19,6 @@ const QuizOptions: React.FC<QuizOptionsProps> = ({
   const [flippedIndices, setFlippedIndices] = useState<Set<number>>(new Set());
 
   useEffect(() => {
-    // Agrega un tiempo de espera antes de voltear todas las tarjetas
     const timer = setTimeout(() => {
       setFlippedIndices(new Set(options.map((_, index) => index)));
     }, 3000);
@@ -27,31 +26,71 @@ const QuizOptions: React.FC<QuizOptionsProps> = ({
     return () => clearTimeout(timer);
   }, [options]);
 
+  const cardVariants = {
+    initial: {
+      rotateY: 0,
+      y: ["-20px", "20px"],
+      rotate: [-1, 0],
+      scale: [1, 1.01, 1],
+      transition: {
+        duration: 4,
+        ease: "easeInOut",
+        repeat: Infinity,
+        repeatType: "reverse" as "reverse" | "loop" | "mirror" | undefined,
+      },
+    },
+    flipped: {
+      rotateY: 180,
+      y: ["-20px", "20px"],
+      rotate: [1, -1],
+      scale: [1, 1.02, 1],
+      transition: {
+        duration: 1,
+        ease: "easeInOut",
+      },
+    },
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-full w-full">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-8">
         {options.map((option, index) => (
-          <motion.div key={option.id}
-            initial={false}
-            animate={{ rotateY: flippedIndices.has(index) ? 180 : 0 }}
-            transition={{ duration: 1, ease: "easeInOut" }}
+          <motion.div
+            key={option.id}
+            variants={cardVariants}
+            initial="initial"
+            animate={flippedIndices.has(index) ? "flipped" : "initial"}
             className="rounded-lg border-8 border-blue-300 shadow-lg cursor-pointer"
-            style={{ width: "calc(90vw / 3)", height: "calc(90vw / 3)", position: "relative", transformStyle: "preserve-3d"}}
+            style={{
+              width: "calc(90vw / 3)",
+              height: "calc(90vw / 3)",
+              position: "relative",
+              transformStyle: "preserve-3d",
+            }}
           >
-            {/* Cara frontal de la tarjeta */}
             <div
               className="absolute w-full h-full flex justify-center items-center bg-gradient-to-b from-blue-700 to-blue-800 text-white text-shadow text-4xl font-bold"
-              style={{ backfaceVisibility: "hidden", transform: "rotateY(0deg)", fontSize: "13rem" }}
+              style={{
+                backfaceVisibility: "hidden",
+                transform: "rotateY(0deg)",
+                fontSize: "13rem",
+              }}
             >
               {option.letter}
             </div>
-            {/* Cara trasera de la tarjeta */}
             <div
               className="absolute w-full h-full flex justify-center items-center"
-              style={{ backfaceVisibility: "hidden", transform: "rotateY(180deg)", position: "absolute" }}
+              style={{
+                backfaceVisibility: "hidden",
+                transform: "rotateY(180deg)",
+              }}
               onClick={() => onOptionSelected(option.id)}
             >
-              <img src={option.imagePath} alt={`Option ${option.letter}`} className="object-cover w-full h-full" />
+              <img
+                src={option.imagePath}
+                alt={`Option ${option.letter}`}
+                className="object-cover w-full h-full"
+              />
             </div>
           </motion.div>
         ))}
