6ce5115670f65240175e906cf20525cbedf64267
Irving Zamora <irvingzamora28@gmail.com>
2024-03-20 18:41:56 -0600

    feat: Enhance quiz option interactivity and display text options

--- src/components/QuizOptions.tsx
+++ src/components/QuizOptions.tsx
@@ -16,12 +16,17 @@ const QuizOptions: React.FC<QuizOptionsProps> = ({
   onOptionSelected,
 }) => {
   const [flippedIndices, setFlippedIndices] = useState<Set<number>>(new Set());
+  const [showOptionText, setShowOptionText] = useState(false);
 
   useEffect(() => {
     if (!shouldFlip) {
       setFlippedIndices(new Set());
+      setShowOptionText(false);
     } else {
       setFlippedIndices(new Set(options.map((_, index) => index)));
+      setTimeout(() => {
+        setShowOptionText(true);
+      }, 800);
     }
   }, [options, shouldFlip]); // Responde a los cambios en shouldFlip y options
 
@@ -53,46 +58,60 @@ const QuizOptions: React.FC<QuizOptionsProps> = ({
     <div className="flex flex-col items-center justify-center h-full w-full">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-8">
         {options.map((option, index) => (
-          <motion.div
-            key={option.key}
-            variants={cardVariants}
-            initial="initial"
-            animate={
-              flippedIndices.has(index) && shouldFlip ? "flipped" : "initial"
-            }
-            className="rounded-lg border-8 border-blue-300 shadow-lg cursor-pointer"
-            style={{
-              width: "calc(90vw / 3)",
-              height: "calc(90vw / 3)",
-              position: "relative",
-              transformStyle: "preserve-3d",
-            }}
-          >
-            <div
-              className="absolute w-full h-full flex justify-center items-center bg-gradient-to-b from-blue-700 to-blue-800 text-white text-shadow text-4xl font-bold"
+          <>
+            <motion.div
+              key={option.key}
+              variants={cardVariants}
+              initial="initial"
+              animate={
+                flippedIndices.has(index) && shouldFlip ? "flipped" : "initial"
+              }
+              className="rounded-lg border-8 border-blue-300 shadow-lg cursor-pointer"
               style={{
-                backfaceVisibility: "hidden",
-                transform: "rotateY(0deg)",
-                fontSize: "13rem",
+                width: "calc(90vw / 3)",
+                height: "calc(90vw / 3)",
+                position: "relative",
+                transformStyle: "preserve-3d",
               }}
             >
-              {String.fromCharCode(65 + index)}
-            </div>
-            <div
-              className="absolute w-full h-full flex justify-center items-center"
-              style={{
-                backfaceVisibility: "hidden",
-                transform: "rotateY(180deg)",
-              }}
-              onClick={() => onOptionSelected(option.key)}
-            >
-              <img
-                src={`/images/${triviaPath}/${option.image}`}
-                alt={`Option ${option.key}`}
-                className="object-cover w-full h-full"
-              />
-            </div>
-          </motion.div>
+              <div
+                className="absolute w-full h-full flex justify-center items-center bg-gradient-to-b from-blue-700 to-blue-800 text-white text-shadow text-4xl font-bold"
+                style={{
+                  backfaceVisibility: "hidden",
+                  transform: "rotateY(0deg)",
+                  fontSize: "13rem",
+                }}
+              >
+                {String.fromCharCode(65 + index)}
+              </div>
+              <div
+                className="absolute w-full h-full flex justify-center items-center"
+                style={{
+                  backfaceVisibility: "hidden",
+                  transform: "rotateY(180deg)",
+                }}
+                onClick={() => onOptionSelected(option.key)}
+              >
+                <img
+                  src={`/images/${triviaPath}/${option.image}`}
+                  alt={`Option ${option.key}`}
+                  className="object-cover w-full h-full"
+                />
+              </div>
+              {showOptionText && (
+                <div className="flex flex-col items-center justify-end w-full absolute -bottom-20 left-0 right-0 px-2 py-2 bg-white rounded-full shadow-lg">
+                  <span
+                    className="text-6xl font-bold text-slate-900 text-shadow"
+                    style={{
+                      transform: "rotateY(180deg)",
+                    }}
+                  >
+                    {option.text}
+                  </span>
+                </div>
+              )}
+            </motion.div>
+          </>
         ))}
       </div>
     </div>
