d42ac3c82e292405caef04ec222845c9b16fa7af
Irving Zamora <irvingzamora28@gmail.com>
2024-04-10 20:03:37 -0600

    feat: Update time limit and improve code structure in GuessWhatHappensQuiz component

--- src/components/GuessWhatHappensQuiz.tsx
+++ src/components/GuessWhatHappensQuiz.tsx
@@ -69,7 +69,7 @@ const GuessWhatHappensQuiz: React.FC<QuizProps> = ({
     const timerRef = useRef<NodeJS.Timeout | null>(null);
     const [isOptionsVisible, setIsOptionsVisible] = useState(false);
     const currentQuestion = triviaQuestions[currentQuestionIndex];
-    const timeLimit = 5000; // seconds
+    const timeLimit = 6; // seconds
 
     // Cleanup timer to avoid memory leaks
     useEffect(() => {
@@ -136,12 +136,16 @@ const GuessWhatHappensQuiz: React.FC<QuizProps> = ({
         setSelectedAnswer(optionId);
     };
 
-    return  (
+    return (
         <div className="text-center w-full h-screen flex flex-col overflow-hidden relative">
             {/* Video Section: Center the video in its designated area */}
-            <div className="video-section flex justify-center items-center" style={{ height: '50%' }}>
+            <div
+                className="video-section flex justify-center items-center "
+                style={{ height: "50%" }}
+            >
                 <video
                     ref={videoRef}
+                    className="border-8 border-white shadow-lg ml-8 my-12 rounded-lg"
                     controls
                     style={{ maxWidth: "100%", maxHeight: "80%" }}
                 >
@@ -149,96 +153,110 @@ const GuessWhatHappensQuiz: React.FC<QuizProps> = ({
                 </video>
             </div>
             {/* Interactive Section: For Quiz Options and ProgressBar */}
-            <div className="interactive-section flex-1" style={{ minHeight: '50%' }}>
+            <div
+                className="interactive-section flex-1"
+                style={{ minHeight: "50%" }}
+            >
                 {/* Options and ProgressBar Container: Ensure consistent spacing */}
-                <div className="options-progress-container flex flex-col justify-between" style={{ height: '100%' }}>
+                <div
+                    className="options-progress-container flex flex-col justify-between"
+                    style={{ height: "100%" }}
+                >
                     <div className="options-container grid grid-cols-2 gap-x-6 gap-y-6 w-3/4 p-9 mx-auto">
                         {isOptionsVisible &&
-                triviaQuestions[currentQuestionIndex].question &&  triviaQuestions[currentQuestionIndex].options?.map(
-                            (option, index) => (
-                                <motion.div
-                                    key={option.key}
-                                    initial="initial"
-                                    animate={
-                                        selectedAnswer === option.key
-                                            ? answerState === "correct"
-                                                ? "correct"
-                                                : "incorrect"
-                                            : isCheckingAnswer
-                                            ? "incorrect"
-                                            : "floating"
-                                    }
-                                    variants={
-                                        option.key ===
-                                        triviaQuestions[currentQuestionIndex]
-                                            .answer
-                                            ? {
-                                                  ...answerVariants,
-                                                  correct: {
-                                                      ...answerVariants.correct,
-                                                      x: "-50%",
-                                                      y: "-100%",
-                                                  },
-                                              }
-                                            : answerVariants
-                                    }
-                                    style={
-                                        answerState === "correct" &&
-                                        selectedAnswer === option.key
-                                            ? {
-                                                  position: "absolute",
-                                                  top: "50%",
-                                                  left: "50%",
-                                              }
-                                            : {}
-                                    }
-                                >
-                                    {/* ... button with letter and option text ... */}
+                            triviaQuestions[currentQuestionIndex].question &&
+                            triviaQuestions[currentQuestionIndex].options?.map(
+                                (option, index) => (
                                     <motion.div
                                         key={option.key}
-                                        variants={answerVariants}
                                         initial="initial"
                                         animate={
                                             selectedAnswer === option.key
-                                                ? answerState
-                                                : "initial"
+                                                ? answerState === "correct"
+                                                    ? "correct"
+                                                    : "incorrect"
+                                                : isCheckingAnswer
+                                                ? "incorrect"
+                                                : "floating"
                                         }
-                                        className="relative flex items-center justify-start my-2 mx-6 min-h-52"
+                                        variants={
+                                            option.key ===
+                                            triviaQuestions[
+                                                currentQuestionIndex
+                                            ].answer
+                                                ? {
+                                                      ...answerVariants,
+                                                      correct: {
+                                                          ...answerVariants.correct,
+                                                          x: "-50%",
+                                                          y: "-100%",
+                                                      },
+                                                  }
+                                                : answerVariants
+                                        }
+                                        style={
+                                            answerState === "correct" &&
+                                            selectedAnswer === option.key
+                                                ? {
+                                                      position: "absolute",
+                                                      top: "50%",
+                                                      left: "50%",
+                                                  }
+                                                : {}
+                                        }
                                     >
-                                        <div className="flex flex-col items-center justify-center w-full">
-                                            <div className="absolute -left-8 z-10 text-white bg-blue-600 rounded-full w-32 h-32 flex items-center justify-center border-8 border-white shadow-lg">
-                                                <span className="text-6xl font-bold text-shadow">
-                                                    {option.key}
-                                                </span>
+                                        {/* ... button with letter and option text ... */}
+                                        <motion.div
+                                            key={option.key}
+                                            variants={answerVariants}
+                                            initial="initial"
+                                            animate={
+                                                selectedAnswer === option.key
+                                                    ? answerState
+                                                    : "initial"
+                                            }
+                                            className="relative flex items-center justify-start my-2 mx-6 min-h-52"
+                                        >
+                                            <div className="flex flex-col items-center justify-center w-full">
+                                                <div className="absolute -left-8 z-10 text-white bg-blue-600 rounded-full w-32 h-32 flex items-center justify-center border-8 border-white shadow-lg">
+                                                    <span className="text-6xl font-bold text-shadow">
+                                                        {option.key}
+                                                    </span>
+                                                </div>
+                                                <button
+                                                    onClick={() =>
+                                                        !isCheckingAnswer &&
+                                                        handleOptionSelect(
+                                                            option.key
+                                                        )
+                                                    }
+                                                    className="pl-36 pr-4 py-3 w-full min-h-40 text-5xl font-medium text-shadow-sm text-blue-600 bg-white rounded-full transition-colors hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-300 shadow-lg"
+                                                >
+                                                    {option.text}
+                                                </button>
                                             </div>
-                                            <button
-                                                onClick={() =>
-                                                    !isCheckingAnswer &&
-                                                    handleOptionSelect(
-                                                        option.key
-                                                    )
-                                                }
-                                                className="pl-36 pr-4 py-3 w-full min-h-40 text-5xl font-medium text-shadow-sm text-blue-600 bg-white rounded-full transition-colors hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-300 shadow-lg"
-                                            >
-                                                {option.text}
-                                            </button>
-                                        </div>
-
+                                        </motion.div>
                                     </motion.div>
-                                </motion.div>
-                            )
-                        )}
+                                )
+                            )}
                     </div>
 
-            <div className="progress-bar-container flex justify-center items-center mb-16" style={{ visibility: isProgressBarVisible ? 'visible' : 'hidden' }}>
-                <ProgressBar
-                    index={currentQuestionIndex}
-                    isProgressBarAnimating={isProgressBarAnimating}
-                    timeLimit={timeLimit}
-                />
-        </div>
-        </div>
-        </div>
+                    <div
+                        className="progress-bar-container flex justify-center items-center mb-16"
+                        style={{
+                            visibility: isProgressBarVisible
+                                ? "visible"
+                                : "hidden",
+                        }}
+                    >
+                        <ProgressBar
+                            index={currentQuestionIndex}
+                            isProgressBarAnimating={isProgressBarAnimating}
+                            timeLimit={timeLimit}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
